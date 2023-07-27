099aa23becc88ff861e6f74951f802375bd0afda
Burak Türker <pythonu0@gmail.com>
2023-07-28 01:32:09 +0300

    admin settings is complated

--- tweet_app/admin.py
+++ tweet_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from tweet_app.models import tweet
 # Register your models here.
+class TweetAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Nicname Group', {'fields':["nickname"]}),
+        ('Message Group', {'fields':["message"]}),
+    ]
+    
+admin.site.register(tweet,TweetAdmin)
