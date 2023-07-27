03d3e12197af8a09595f9893c9aad5f312143232
Burak Türker <pythonu0@gmail.com>
2023-07-28 00:11:02 +0300

    Inheritance is complated

--- tweet_app/models.py
+++ tweet_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+
+class tweet(models.Model):
+    nickname = models.CharField(max_length=15)
+    message = models.CharField(max_length=130)
+
+    def __str__(self):
+        return f"nickname: {self.nickname} message: {self.message} "
--- tweet_app/views.py
+++ tweet_app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
 def ListTweet(request):
-    return render(request,'tweet_app/ListTweet.html')
+    all_tweet = models.tweet.objects.all()
+    context_tweet ={"tweets":all_tweet }
+    return render(request,'tweet_app/ListTweet.html',context=context_tweet)
 
 
 def AddTweet(request):
