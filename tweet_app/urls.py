727c567a2bc862d21844b3578971025d2534e295
Burak Türker <pythonu0@gmail.com>
2023-07-29 15:50:42 +0300

    Tweet App is complated

--- tweet_app/urls.py
+++ tweet_app/urls.py
@@ -5,5 +5,7 @@ app_name ="tweet_app"
 
 urlpatterns = [
     path('',views.ListTweet,name='listtweet'),
-    path('addtweet/',views.AddTweet,name='addtweet')
+    path('addtweet/',views.AddTweet,name='addtweet'),
+    path('signup/',views.SignUpView.as_view(), name="signup"),
+    path('deletetweet/<int:id>',views.Tweet_delete,name="deletetweet")
 ]
--- tweet_app/views.py
+++ tweet_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render,redirect
 from . import models
-from django.urls import reverse
+from django.urls import reverse,reverse_lazy
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView 
 
 # Create your views here.
 def ListTweet(request):
@@ -12,9 +14,21 @@ def ListTweet(request):
 @login_required(login_url="/login")
 def AddTweet(request):
     if request.POST:
-       nickname = request.POST["nickname"]
        message =request.POST["message"]
-       models.tweet.objects.create(nickname=nickname,message=message)
+       models.tweet.objects.create(username=request.user,message=message)
        return redirect(reverse('tweet_app:listtweet'))
     else:
         return render(request,'tweet_app/AddTweet.html')
+    
+
+@login_required
+def Tweet_delete(request,id):
+    tweet=models.tweet.objects.get(pk=id)
+    if request.user == tweet.username:
+        models.tweet.objects.filter(id=id).delete()
+        return redirect('tweet_app:listtweet')
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
